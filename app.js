62d35d2579af70cd0b9e874e8538a2a0078536be
Logan Pinel <loganpinel@outlook.com>
2025-09-23 20:58:22 -0400

    Configured static files and nunjucks templating in app.js

--- app.js
+++ app.js
@@ -2,9 +2,21 @@
 
 //Import Modules
 const express = require('express');
+const nunjucks = require('nunjucks');
+const path = require('path');
 
 //create the app
 const app = express();
+
+//serve static files from the 'static' directory
+//these files will be used for frontend functionality
+app.use('/static', express.static(path.join(__dirname, 'static')));
+
+//configure nunjucks templating 
+nunjucks.configure('templates', {
+  autoescape: true,
+  express: app,
+});
 
 //create a basic get route for testing
 app.get('/', (req, res, next) => {
